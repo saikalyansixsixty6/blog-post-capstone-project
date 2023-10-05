ee2e0accc8c2d3b84d1d9be26b7b146660a23e7b
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-10-05 14:42:28 +0000

    Appwrite

--- Blog-post/src/App.jsx
+++ Blog-post/src/App.jsx
@@ -2,11 +2,11 @@
 import './App.css'
 
 function App() {
-  
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">hello React+Vite</h1>
+      <h1 className="text-3xl font-bold underline">hello React</h1>
     </>
   )
 }
