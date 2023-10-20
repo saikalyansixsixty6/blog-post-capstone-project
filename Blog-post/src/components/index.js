c201fa646505ae674da4e6b7e4ddb7ba64c18dfe
Sai Kalyan K <91243096+saikalyansixsixty6@users.noreply.github.com>
2023-10-20 06:22:33 +0000

    AuthLayout

--- Blog-post/src/components/index.js
+++ Blog-post/src/components/index.js
@@ -2,5 +2,9 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Login from "./Login";
+import Button from "./Button";
+import Input from "./Input";
 
-export {Header,Footer,LogoutBtn,Container}
+export {Header,Footer,LogoutBtn,Container,Logo,Login,Button,Input}
